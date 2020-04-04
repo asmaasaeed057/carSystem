be357537e81b0ed2a328c6413ad2631f4fb00a77
asmaasaeed057 <asmaasaeed057@gmail.com>
2020-04-04 13:59:36 +0200

    solve conflict and add account to group

--- resources/views/admin/account/create.blade.php
+++ resources/views/admin/account/create.blade.php
@@ -1,88 +1,129 @@
-@extends('backend::layouts.main')
+@extends('layouts.app')
+
+
+@section('style')
+<link rel="stylesheet" href="{{ asset('FrontEnd') }}/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+@endsection
+
+@section('script')
+<!-- DataTables -->
+<script src="https://kit.fontawesome.com/c099210540.js" crossorigin="anonymous"></script>
+<script src="{{ asset('FrontEnd') }}/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('FrontEnd') }}/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+@endsection
+
+@section('js')
+
+<script>
+    $(function() {
+        $('#example1').DataTable()
+        $('#example2').DataTable({
+            'paging': true,
+            'lengthChange': false,
+            'searching': false,
+            'ordering': true,
+            'info': true,
+            'autoWidth': false
+        })
+    })
+</script>
+@endsection
+
 @section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            {{ trans('site.Dashboard') }}
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">{{ trans('site.Dashboard') }}</li>
+        </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
 
-<div class="page-header">
-    <div class="pull-left"><h1> انشاء</h1></div>
-    <div class="pull-right">
-        <img src="{{asset('themes/theme_back/img/logo.png')}}" alt="" class='retina-ready' width="59" height="49">
-    </div>
-</div>
-<div class="breadcrumbs">
+        <div class="row">
+            <div class="col-xs-12">
 
-    <ul>
-        <li><a href="{{ route('home') }}">الرئيسية</a> <i class="icon-angle-left"></i></li>
-        <li><a href="{{ route('account.index') }}"> الحسابات</a> <i class="icon-angle-left"></i></li>
-        <li><a href="">اضافة</a></li>
-    </ul>
-    <div class="close-bread"><a href="#"><i class="icon-remove"></i></a></div>
+                <div class="box box-primary">
+                    <div class="box-header">
+                        <h3 class="box-title">{{ trans('site.Dashboard') }}</h3>
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="box box-warning">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Create Account</h3>
+                            </div>
+                            <!-- /.box-header -->
+                            <div class="box-body">
+                                <form action="{{ route('account.store')}}" method="POST">
+                                    @csrf
+                                    @method('POST')
 
-</div>
+                                    <!-- text input -->
+                                    <div class="form-group">
+                                        <label>Group </label>
+                                        <div class="controls">
+
+                                            <select class="" name="group_id" required="required">
+                                                <option value="">choose</option>
+                                                @foreach($groups as $group)
+                                                <option value="{{$group->group_id }}">{{$group->group_name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label> Name</label>
+                                        <input type="text" name="name" class="form-control" placeholder="Name">
+
+                                    </div>
+                                    <div class="form-group">
+
+                                        <label>Email</label>
+                                        <input type="text" name="email" class="form-control" placeholder="Email">
+
+                                    </div>
+                                    <div class="form-group">
+
+                                        <label>Password</label>
+                                        <input type="text" name="password" class="form-control" placeholder="password">
+
+                                    </div>
+                                    <div class="form-group">
+
+                                        <label>Password Confirmation</label>
+                                        <input type="text" name="password_confirmation" class="form-control">
+
+                                    </div>
 
 
-<div class="row-fluid">
-    <div class="span12">
-        <div class="box box-bordered">
-            <div class="box-title">
-                <h3><i class="icon-th-list"> </i>اضافة</h3>
+
+                                    <input type="submit" class="btn-primary" value="{{ trans('site.add') }}">
+
+                                </form>
+                            </div>
+                            <!-- /.box-body -->
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
             </div>
-
-            <div class="box-content nopadding">
-
-                <form enctype="multipart/form-data"  class="form-horizontal form-bordered form-validate" id="pages-form" action="{{ route('register') }}" method="post">
-                    @csrf
-                    <div class="control-group">
-                        <label class="control-label required" for="Name">المجموعه  <span class="required">*</span></label>           
-                        <div class="controls">
-
-                            <select class="" name="group_id" required="required">
-                                <option value="">اختار</option>
-                                @foreach($groups as $group)
-                                <option value="{{$group->group_id }}">{{$group->group_name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-
-                    <div class="control-group">
-                        <label class="control-label required" for="Name">الاسم  <span class="required">*</span></label>           
-                        <div class="controls">
-
-                            <input size="60" maxlength="120" name="name" id="" type="text" style="height:20px; width: 220px; float: right;" required="required"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label required" for="Name">البريد الالكترونى  <span class="required">*</span></label>           
-                        <div class="controls">
-
-                            <input size="60" maxlength="120" name="email" id="" type="email" style="height:20px; width: 220px; float: right;" required="required"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label required" for="Name">كلمة المرور  <span class="required">*</span></label>           
-                        <div class="controls">
-
-                            <input size="60" maxlength="120" name="password" id="" type="Password" style="height:20px; width: 220px; float: right;" required="required"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label required" for="Name">اعادة كلمة المرور  <span class="required">*</span></label>           
-                        <div class="controls">
-
-                            <input size="60" maxlength="120" name="password_confirmation" id="" type="password" style="height:20px; width: 220px; float: right;" required="required"/>
-                        </div>
-                    </div>
-
-
-
-
-                    <div class="form-actions">
-                        <input class="btn btn-primary" type="submit" value="انشاء" style="float: right;" /> 
-                    </div>
-
-                </form>
-
-            </div>
+            <!-- /.col -->
         </div>
-    </div>
-    @endsection
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
+@endsection
