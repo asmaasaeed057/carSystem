f5e77c15388cde536537a7e5750f6771c3742d0e
asmaasaeed057 <asmaasaeed057@gmail.com>
2020-04-07 23:23:05 +0200

    add operation order and invoice payment model

---
+++ app/OperationOrder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OperationOrder extends Model
+{
+
+    protected $table = 'operation_orders';
+    protected $fillable = ['operation_order_number' , 'operation_order_date' , 'invoice_id'];
+    protected $primaryKey = 'operation_order_id';
+
+    public function invoice(){
+        return $this->belongsTo('App\Invoice' , 'invoice_id');
+    }
+
+
+}
